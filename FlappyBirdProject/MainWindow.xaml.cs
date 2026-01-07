b3014e3be06996dabaa087a9b4d41fb9ac2c32bf
VatapiXD <vatapi.2005@gmail.com>
2026-01-07 10:28:55 +0100

    Ugralas kesz

--- FlappyBirdProject/MainWindow.xaml.cs
+++ FlappyBirdProject/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace FlappyBirdProject
 		private DispatcherTimer gameTimer = new DispatcherTimer();
 		private double birdVelocity = 0;
 		private double gravity = 0.5;
-		private double flapStrength = -8;
+		private double jumpStrenght = -8;
+		private bool gameOver = false;
 		public MainWindow()
         {
             InitializeComponent();
@@ -33,12 +34,18 @@ namespace FlappyBirdProject
 
 		private void Window_KeyDown(object sender, KeyEventArgs e)
 		{
-			
+			if (!gameOver && e.Key == Key.Space)
+			{
+				birdVelocity = jumpStrenght;
+			}
 		}
 
-		private void GameLoop(object sender, KeyEventArgs e)
+		private void GameLoop(object sender, EventArgs e)
 		{
-			
+			birdVelocity += gravity;
+			double newTop = Canvas.GetTop(Bird) + birdVelocity;
+			Canvas.SetTop(Bird, newTop);
+
 		}
 	}
 }
