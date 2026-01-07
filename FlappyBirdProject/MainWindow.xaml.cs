8dae85c8d4490be7c1aef051b29eded1be48a5b9
VatapiXD <vatapi.2005@gmail.com>
2026-01-07 10:01:21 +0100

    Add project files.

---
+++ FlappyBirdProject/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FlappyBirdProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		private DispatcherTimer gameTimer = new DispatcherTimer();
+		private double birdVelocity = 0;
+		private double gravity = 0.5;
+		private double flapStrength = -8;
+		public MainWindow()
+        {
+            InitializeComponent();
+
+			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+			gameTimer.Tick += GameLoop;
+			gameTimer.Start();
+
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			
+		}
+
+		private void GameLoop(object sender, KeyEventArgs e)
+		{
+			
+		}
+	}
+}
