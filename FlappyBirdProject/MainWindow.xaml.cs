95a51eebbc400ab2e7802d6758c3f4e7f68563f6
VatapiXD <vatapi.2005@gmail.com>
2026-01-14 10:39:59 +0100

    Pipe + scoring

--- FlappyBirdProject/MainWindow.xaml.cs
+++ FlappyBirdProject/MainWindow.xaml.cs
@@ -19,9 +19,15 @@ namespace FlappyBirdProject
     {
 		private DispatcherTimer gameTimer = new DispatcherTimer();
 		private double birdVelocity = 0;
-		private double gravity = 0.5;
-		private double jumpStrenght = -8;
+		private double gravity = 0.9;
+		private double jumpStrenght = -10;
 		private bool gameOver = false;
+
+		List<PipePair> pipes = new List<PipePair>();
+		Random random = new Random();
+		int pipeCounter = 50;
+		int score = 0;
+
 		public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +35,7 @@ namespace FlappyBirdProject
 			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
 			gameTimer.Tick += GameLoop;
 			gameTimer.Start();
+
 
 		}
 
@@ -46,6 +53,68 @@ namespace FlappyBirdProject
 			double newTop = Canvas.GetTop(Bird) + birdVelocity;
 			Canvas.SetTop(Bird, newTop);
 
+			pipeCounter++;
+
+			if (pipeCounter > 50)
+			{
+				pipes.Add(new PipePair(GameCanvas, random));
+				pipeCounter = 0;
+			}
+
+			Rect birdRect = GetBirdRect();
+			foreach (var pipe in pipes){
+				pipe.Move();
+
+				if(pipe.CheckCollision(birdRect))
+				{
+					EndGame();
+					return;
+				}
+				if(pipe.Passed(Canvas.GetLeft(Bird)))
+				{
+					score++;
+				}
+			}
+
+			Rect groundRect = GetGroundRect();
+			if (birdRect.IntersectsWith(groundRect))
+			{
+				EndGame();
+				return;
+			}
+		}
+
+		private void EndGame()
+		{
+			gameTimer.Stop();
+			gameOver = true;
+			MessageBox.Show("Game Over! Your score: " + score);
+		}
+
+
+		private Rect GetBirdRect()
+		{
+			double collisionBox = 30;
+			double offsetX = (Bird.Width - collisionBox) / 2;
+			double offsetY = (Bird.Height - collisionBox) / 2;
+
+
+			return new Rect(
+				Canvas.GetLeft(Bird) + offsetX,
+				Canvas.GetTop(Bird) + offsetY,
+				collisionBox,
+				collisionBox
+			);
+		}
+
+		private Rect GetGroundRect()
+		{
+			double left = Canvas.GetLeft(GroundImage);
+			double top = Canvas.GetTop(GroundImage);
+			double width = GroundImage.Width;
+			double height = GroundImage.Height;
+
+			return new Rect(left, top, width, height);
 		}
 	}
 }
---
+++ FlappyBirdProject/PipePair.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlappyBirdProject
+{
+	internal class PipePair
+	{
+		public Rectangle TopPipe { get; private set; }
+		public Rectangle BottomPipe { get; private set; }
+		public bool Scored { get; set; }
+		public double speed = 6;
+
+		public PipePair(Canvas canvas, Random random)
+		{
+			int gapHeight = 150;
+			int topHeight = random.Next(50, 250);
+
+			ImageBrush topBrush = new ImageBrush
+			{
+				ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/pipe.png")),
+				Stretch = Stretch.Fill,
+				RelativeTransform = new RotateTransform(180, 0.5, 0.5)
+			};
+
+			TopPipe = new Rectangle
+			{
+				Width = 80,
+				Height = topHeight,
+				Fill = topBrush,
+			};
+
+
+			ImageBrush bottomBrush = new ImageBrush
+			{
+				ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/pipe.png")),
+				Stretch = Stretch.Fill
+			};
+
+			BottomPipe = new Rectangle
+			{
+				Width = 80,
+				Height = canvas.Height - topHeight - gapHeight,
+				Fill = bottomBrush,
+			};
+
+			Canvas.SetLeft(TopPipe, canvas.ActualWidth);
+			Canvas.SetTop(TopPipe, 0);
+
+			Canvas.SetLeft(BottomPipe, canvas.ActualWidth);
+			Canvas.SetTop(BottomPipe, topHeight + gapHeight);
+
+			canvas.Children.Add(TopPipe);
+			canvas.Children.Add(BottomPipe);
+		}
+
+		public bool Passed(double birdX)
+		{
+			if(!Scored && Canvas.GetLeft(TopPipe) + TopPipe.Width < birdX) { 
+				Scored = true;
+				return true;
+			}
+			return false;
+		}
+
+		public void Move()
+		{
+			Canvas.SetLeft(TopPipe, Canvas.GetLeft(TopPipe) - speed);
+			Canvas.SetLeft(BottomPipe, Canvas.GetLeft(BottomPipe) - speed);
+		}
+
+
+		public bool CheckCollision(Rect birdRect)
+		{
+			Rect topRect = GetRect(TopPipe);
+			Rect bottomRect = GetRect(BottomPipe);
+
+			return birdRect.IntersectsWith(topRect) ||
+				   birdRect.IntersectsWith(bottomRect);
+		}
+
+		private Rect GetRect(Rectangle r)
+		{
+			return new Rect(
+				Canvas.GetLeft(r),
+				Canvas.GetTop(r),
+				r.Width,
+				r.Height
+			);
+		}
+
+	}		
+}
