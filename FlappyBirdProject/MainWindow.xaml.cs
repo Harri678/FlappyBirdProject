51406d498da588be325ad43f3247842b970cd871
VatapiXD <vatapi.2005@gmail.com>
2026-01-21 10:34:23 +0100

    kod, eső, end screen elkészítve

--- FlappyBirdProject/MainWindow.xaml.cs
+++ FlappyBirdProject/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using WpfAnimatedGif;
 
 namespace FlappyBirdProject
 {
@@ -23,6 +24,19 @@ namespace FlappyBirdProject
 		private double jumpStrenght = -10;
 		private bool gameOver = false;
 
+		private double normalJump = -10;
+		private double rainJump = -5;
+
+		private bool fogEnabled = false;
+
+		bool fogActive = false;
+		int fogTimer = 0;
+
+		int fogOnTime = 300;
+		int fogOffTime = 100;
+
+
+
 		List<PipePair> pipes = new List<PipePair>();
 		Random random = new Random();
 		int pipeCounter = 50;
@@ -32,9 +46,17 @@ namespace FlappyBirdProject
         {
             InitializeComponent();
 
+			var gif = new BitmapImage();
+			gif.BeginInit();
+			gif.UriSource = new Uri("pack://application:,,,/Images/RainGif.gif");
+			gif.CacheOption = BitmapCacheOption.OnLoad;
+			gif.EndInit();
+
+			ImageBehavior.SetAnimatedSource(RainLayer, gif);
+
 			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
 			gameTimer.Tick += GameLoop;
-			gameTimer.Start();
+			//gameTimer.Start();
 
 
 		}
@@ -47,6 +69,44 @@ namespace FlappyBirdProject
 			}
 		}
 
+		private void StartGame_Click(object sender, RoutedEventArgs e)
+		{
+			MenuPanel.Visibility = Visibility.Hidden;
+			Canvas.SetLeft(Bird, 100);
+			Canvas.SetTop(Bird, 200);
+
+			gameTimer.Start();
+		}
+
+		//Eső
+		private void RainToggle_Checked(object sender, RoutedEventArgs e)
+		{
+			RainLayer.Visibility = Visibility.Visible;
+			jumpStrenght = rainJump;
+		}
+
+		private void RainToggle_Unchecked(object sender, RoutedEventArgs e)
+		{
+			RainLayer.Visibility = Visibility.Hidden;
+			jumpStrenght = normalJump;
+		}
+
+		//Köd
+		private void FogToggle_Checked(object sender, RoutedEventArgs e)
+		{
+			fogEnabled = true;
+			fogActive = true;
+			FogLayer.Visibility = Visibility.Visible;
+		}
+
+		private void FogToggle_Unchecked(object sender, RoutedEventArgs e)
+		{
+			fogEnabled = false;
+			FogLayer.Visibility = Visibility.Hidden;
+		}
+
+
+		//Game loop
 		private void GameLoop(object sender, EventArgs e)
 		{
 			birdVelocity += gravity;
@@ -61,26 +121,45 @@ namespace FlappyBirdProject
 				pipeCounter = 0;
 			}
 
-			Rect birdRect = GetBirdRect();
-			foreach (var pipe in pipes){
-				pipe.Move();
+				Rect birdRect = GetBirdRect();
+				foreach (var pipe in pipes)
+				{
+					pipe.Move();
 
-				if(pipe.CheckCollision(birdRect))
+					if (pipe.CheckCollision(birdRect))
+					{
+						EndGame();
+						return;
+					}
+					if (pipe.Passed(Canvas.GetLeft(Bird)))
+					{
+						score++;
+					}
+				}
+
+				Rect groundRect = GetGroundRect();
+				if (birdRect.IntersectsWith(groundRect))
 				{
 					EndGame();
 					return;
 				}
-				if(pipe.Passed(Canvas.GetLeft(Bird)))
-				{
-					score++;
-				}
-			}
 
-			Rect groundRect = GetGroundRect();
-			if (birdRect.IntersectsWith(groundRect))
+			if (fogEnabled)
 			{
-				EndGame();
-				return;
+				fogTimer++;
+
+				if (!fogActive && fogTimer > fogOffTime)
+				{
+					fogActive = true;
+					FogLayer.Visibility = Visibility.Visible;
+					fogTimer = 0;
+				}
+				else if (fogActive && fogTimer > fogOnTime)
+				{
+					fogActive = false;
+					FogLayer.Visibility = Visibility.Hidden;
+					fogTimer = 0;
+				}
 			}
 		}
 
@@ -88,9 +167,31 @@ namespace FlappyBirdProject
 		{
 			gameTimer.Stop();
 			gameOver = true;
-			MessageBox.Show("Game Over! Your score: " + score);
+
+			ScoreText.Text = $"Score: {score}";
+			GameOverPanel.Visibility = Visibility.Visible;
 		}
 
+		private void Restart_Click(object sender, RoutedEventArgs e)
+		{
+			GameOverPanel.Visibility = Visibility.Hidden;
+			MenuPanel.Visibility = Visibility.Visible;
+			Canvas.SetLeft(Bird, 100);
+			Canvas.SetTop(Bird, 200);
+			birdVelocity = 0;
+
+			foreach (var pipe in pipes)
+			{
+				GameCanvas.Children.Remove(pipe.TopPipe);
+				GameCanvas.Children.Remove(pipe.BottomPipe);
+			}
+			pipes.Clear();
+
+			score = 0;
+			pipeCounter = 0;
+
+			gameOver = false; 
+		}
 
 		private Rect GetBirdRect()
 		{
